9c0767184564de79c1cc67a56caefbbd2d77cd46
Helena Picinin <137627315+Helengendary@users.noreply.github.com>
2024-07-11 08:51:02 -0300

    Create testes.py

---
+++ Estudar/arquivos/testes.py
@@ -0,0 +1,36 @@
+import math
+
+i = math.log2(2)
+
+variavel = math.sqrt(9)
+
+numero_de_pi = math.pi
+
+print (i)
+print (variavel)
+print (numero_de_pi)
+print ("vlw, flw")
+
+float  = 0.6
+float_teste = 1.12
+
+print (f"o numero é {float_teste}")
+print (float)
+
+lista = [1, 3, 5, 6, 7, 8, 9]
+
+print (lista[4]) 
+
+tupla = (1, 2, 3, 4, 6)
+
+print (tupla[4])
+
+sala_de_aula = {"Diva": "Helena", 
+                "Alunas com na": ["milena", "mariana", "juliana", "sabrina", "helena"]}
+
+print (sala_de_aula["Diva"])
+print (sala_de_aula["Alunas com na"])
+
+musica = "Collard Greens"
+
+print ("Minha música do momento é " + musica)
